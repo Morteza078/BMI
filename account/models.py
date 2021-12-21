9f191cde45d74e73f60e4b91346acc3f912c6692
morteza078 <mortezarobatjazi.77@gmail.com>
2021-12-21 14:52:05 -0800

    add models  of site

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class CustomUser(AbstractUser):
+    length = models.FloatField(default=0)
+    weight = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name = "Custom user"
+        verbose_name_plural = "Custom users"
