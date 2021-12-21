48ad9df9cd00eeb3d87cbc27f3eb082613a270bd
morteza078 <mortezarobatjazi.77@gmail.com>
2021-12-21 15:06:44 -0800

    add view for  homepage

--- health/views.py
+++ health/views.py
@@ -1,9 +1,22 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
 
+from health.forms import BMIForm
+
 
 class IndexView(View):
     def get(self, request):
-        return render(request, "health/index.html",)
+        return render(request, "health/index.html", )
+
+
+class BMIView(LoginRequiredMixin,View):
+    form_class = BMIForm
+    template_name = 'health/bmi.html'
+    initial = {'key': 'value'}
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
