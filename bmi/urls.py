36d3e6c21e3e01991ee3a13317a064001f31b195
morteza078 <mortezarobatjazi.77@gmail.com>
2021-12-21 14:59:23 -0800

    add base urls

--- bmi/urls.py
+++ bmi/urls.py
@@ -20,4 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('health.urls')),
+    path('auth/', include('account.urls')),
+    path('api/', include('core.api.urls')),
 ]
