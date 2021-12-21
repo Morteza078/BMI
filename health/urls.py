0f9b71958f4442944b63a63bfd0078312c22643c
morteza078 <mortezarobatjazi.77@gmail.com>
2021-12-21 15:02:40 -0800

    add url for homepage

--- health/urls.py
+++ health/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, BMIView
 
 app_name = 'health'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('bmi/', BMIView.as_view(), name='BMI'),
 ]
