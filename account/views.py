086466435f4e6500b9c1b438a6a8c989677875ba
morteza078 <mortezarobatjazi.77@gmail.com>
2021-12-21 15:04:50 -0800

    add view for login and displaying register form

--- account/views.py
+++ account/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+
+from .forms import UserLoginForm, RegisterForm
+
 
 # Create your views here.
+
+
+class UserLoginView(LoginView):
+    authentication_form = UserLoginForm
+    template_name = "account/login.html"
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if 'next' in request.GET:
+                return HttpResponseRedirect(request.GET['next'])
+            return redirect('health:index')
+        return super().get(request, *args, **kwargs)
+
+
+class RegisterView(View):
+    form_class = RegisterForm
+    template_name = 'account/register.html'
+    initial = {'key': 'value'}
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
