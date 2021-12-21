76206d8aff49ec462206853c746d042cb756708f
morteza078 <mortezarobatjazi.77@gmail.com>
2021-12-21 14:55:53 -0800

    create  register.js for sending of register form

---
+++ static/auth/js/register.js
@@ -0,0 +1,33 @@
+$('#register_form').on('submit', function (e) {
+    let username = $('#id_username').val()
+    let password = $('#id_password').val()
+    let confirm_password = $('#id_confirm_password').val()
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: 'http://127.0.0.1:8000/api/account/register/',
+        data: JSON.stringify({
+            username: username,
+            password: password,
+            confirm_password: confirm_password
+        }),
+        success: function (data) {
+            alert(data['username'] + ' ' + 'register is success')
+            $('#non-field-error').html(' ')
+        },
+        error: function (xhr, errmsg, err) {
+            console.log(JSON.parse(xhr.responseText))
+            let non_field_errors = JSON.parse(xhr.responseText)
+            $('#non-field-error').html(non_field_errors['non_field_errors'])
+            $('#password-error').html(non_field_errors['password'])
+            $('#confirm-password-error').html(non_field_errors['confirm_password'])
+            $('#register_form').click(function () {
+                $('#password-error').html('') ;
+                $('#confirm-password-error').html('')
+            });
+
+        },
+        contentType: "application/json",
+        dataType: 'json',
+    });
+});
